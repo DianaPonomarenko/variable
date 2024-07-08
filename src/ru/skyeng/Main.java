2fad74b9a1fa199ece4660042fedaea71047622b
Diana <diananovaya2015@mail.ru>
2024-07-09 00:08:41 +0300

    variables finish

--- src/ru/skyeng/Main.java
+++ src/ru/skyeng/Main.java
@@ -111,10 +111,141 @@ public class Main {
         var totaltime = 640;
         var time = 8;
         var employeeInCompany = totaltime / time;
-        System.out.println("Всего работников в компании " + employeeInCompany + " человек!" );
+        System.out.println("Всего работников в компании "
+                + employeeInCompany + " человек!" );
         var employeeInCompany2 = employeeInCompany + 94;
-        System.out.println("Если в компании работает на 94 чел больше " + employeeInCompany2 + " человек!");
+        System.out.println("Если в компании работает на 94 чел больше "
+                + employeeInCompany2 + " человек!");
         var totaltime2 = employeeInCompany2 * 8;
         System.out.println("Если в компании работает 174 человек то " + totaltime2 + " часов работы может быть поделено между сотрудниками ! ");
+
+        short bananas = 200;
+        System.out.println("Бананов " + bananas + " кг");
+
+        float sugar = 3;
+        float onePortion = sugar / 4;
+        System.out.println("Одна порция сахара весит " + onePortion + " кг");
+
+        byte a1 = 1;
+        short b1 = 1;
+        int c1 = 1;
+        int d = a1 + b1 + c1;
+         System.out.println(d);
+
+         float g1 = a1 + 1f;
+         System.out.println(g1);
+
+         int a2 = 1;
+         byte b2 = 10;
+         float  c2 = 4.57f;
+         System.out.println(a2);
+         System.out.println(b2);
+         System.out.println(c2);
+
+         float a3 = 27.12f;
+         long b3 = 9876789554L;
+         float c3 = 2.786f;
+         int v2 = 569;
+         short g2 = -159;
+         short h3 = 27897;
+         byte f2 = 67;
+        System.out.println(a3);
+        System.out.println(b3);
+        System.out.println(c3);
+        System.out.println(v2);
+        System.out.println(g2);
+        System.out.println(h3);
+        System.out.println(f2);
+
+        byte LP = 23 * 3;
+        System.out.println("У Людмилы Павловны " + LP+  " учеников" );
+        byte AS = 27 * 3;
+        System.out.println("У Анны Сергеевны " + AS +  " учеников" );
+        byte  EA = 30 * 3;
+        System.out.println("У Екатерины Андреевны " + EA +  " учеников"  );
+        int totalStudents= LP + AS + EA;
+        System.out.println("Всего учеников " + totalStudents );
+        int totalSheetsOfPaper = 480;
+        System.out.println("Всего листов бумаги " + totalSheetsOfPaper + " листов");
+        float numberOfSheetsOfPaperForEachStudent = totalSheetsOfPaper / totalStudents ;
+        System.out.println("Количество листов бумаги на каждого ученика по " + numberOfSheetsOfPaperForEachStudent +" Шт");
+
+        byte machinePerformanceInOneMinutes = 16 / 2;
+        System.out.println("Производительность машины за одну минуты " + machinePerformanceInOneMinutes + " шт.бутылок");
+        int machinePerformanceInTwentyMinutes = machinePerformanceInOneMinutes * 20;
+        System.out.println("Производительность машины за 20 минуты " + machinePerformanceInTwentyMinutes + " шт.бутылок" );
+        int machinePerformanceForDay = machinePerformanceInOneMinutes * 1440;
+        System.out.println("Производительность машины за сутки " + machinePerformanceForDay + " шт. бутылок");
+        int machinePerformanceFor3Day = machinePerformanceForDay * 3;
+        System.out.println("Производительность машины за 3 дня " + machinePerformanceFor3Day+ " шт. бутылок");
+        int  machinePerformanceForMonth = machinePerformanceFor3Day * 10;
+        System.out.println("Производительность машины за месяц " + machinePerformanceForMonth+ " шт. бутылок");
+
+        int totalCans = 120;
+        System.out.println("Всего банок " + totalCans + " нужно для школы");
+        byte cansOfWhitePaintForOneClass = 2;
+        System.out.println( "Для одного класса понадобиться " + cansOfWhitePaintForOneClass + " банки с белой краской");
+        byte cansOfBrownPaintForOneClass = 4;
+        System.out.println( "Для одного класса понадобиться " + cansOfBrownPaintForOneClass + " банки с коричневой краской");
+        int totalCansOneClass = cansOfWhitePaintForOneClass + cansOfBrownPaintForOneClass;
+        System.out.println("Для одного класса понадобиться " + totalCansOneClass  + " банок с краской");
+        int totalClasses = totalCans / totalCansOneClass;
+        System.out.println("Всего классов " + totalClasses);
+        int totalBrownСans = totalClasses * cansOfBrownPaintForOneClass;
+        System.out.println("В школе, где 20 классов нужном " + totalBrownСans + " банок с коричневой краской");
+        int totalWhiteСans = totalClasses * cansOfWhitePaintForOneClass;
+        System.out.println("В школе, где 20 классов нужном " + totalWhiteСans + " банок с белой краской");
+
+        byte bananas1 = 80;
+        System.out.println("Один банан весит " + bananas1 + " гр." );
+        int bananasForBreakfast5 = bananas1 * 5;
+        System.out.println("Вес бананов для завтрака " + bananasForBreakfast5 + " гр.");
+        byte Milk100 = 105;
+        System.out.println("100 мл. молока весит " + Milk100 +" гр.");
+        int MilkForBreakfast = Milk100 * 2;
+        System.out.println("Вес молока для завтрака " + MilkForBreakfast  + " гр.");
+        byte iceСream = 100;
+        System.out.println("Одна порция мороженого весит " + iceСream +" гр.");
+        int iceСreamForBreakfast = iceСream * 2;
+        System.out.println("Вес мороженого для завтрака " + iceСreamForBreakfast  + " гр.");
+        byte egg = 70;
+        System.out.println("Одно яйцо весит " + egg  + " гр.");
+        int eggsForBreakfast = egg * 4;
+        System.out.println("Вес яиц для завтрака " + eggsForBreakfast + " гр.");
+        int Breakfast = bananasForBreakfast5 + MilkForBreakfast + iceСreamForBreakfast + eggsForBreakfast;
+        System.out.println("Вес спортзавтрака " + Breakfast + " гр.");
+        byte gram = 1;
+        System.out.println("Один грамм " + gram + " гр.");
+        double kilogram = gram * 1000;
+        System.out.println("Один киллограмм " + kilogram + " гр." );
+        double BreakfastKg = Breakfast / kilogram;
+        System.out.println("Вес спортзавтрака " + BreakfastKg + " кг.");
+
+        int salaryM = 67760;
+        System.out.println("Зарплата Маши " + salaryM + " руб. в мес.");
+        int salaryD= 83690;
+        System.out.println("Зарплата Дениса " + salaryD + " руб. в мес.");
+        int salaryK = 76230;
+        System.out.println("Зарплата Кристины " + salaryK  + " руб. в мес.");
+        int salaryEmployees = salaryM + salaryD + salaryK;
+        System.out.println("Зарплата всех сотрудников " + salaryEmployees  + " руб. в мес." );
+        int salaryEmployeesYear = salaryEmployees * 12;
+        System.out.println("Зарплата всех сотрудников " + salaryEmployeesYear  + " руб. в год" );
+        double increasePerYear = 1.1;
+        int months = 12;
+        double salaryMUp = salaryM * increasePerYear;
+        double salaryDUp = salaryD * increasePerYear;
+        double salaryKUp = salaryK * increasePerYear;
+        double salaryResult = salaryMUp * months;
+        System.out.println( "ЗП Маши за год составляет с надбавкой " + salaryResult + "руб. в год" );
+        double salaryResult2 = salaryDUp * months;
+        System.out.println( "ЗП Денисе за год составляет с надбавкой " + salaryResult2 + "руб. в год" );
+        double salaryResult3 = salaryKUp * months;
+        System.out.println( "ЗП Кристины за год составляет с надбавкой " + salaryResult3 + "руб. в год" );
+        double salaryEmployeesYear2 = salaryResult + salaryResult2 + salaryResult3;
+        System.out.println("Зарплата всех сотрудников с надбавкой " + salaryEmployeesYear  + " руб. в год" );
+        double salaryDifference = salaryEmployeesYear2 - salaryEmployeesYear;
+        System.out.println("годовой доход всех сотрудников вырос на " + salaryEmployeesYear  + " руб. в год" );
+
     }
 }
